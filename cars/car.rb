17af2c5bacc43d7fa6c5715ba33af67a0fba12d7
nevroje <joakim.berglind@gmail.com>
2019-01-25 20:33:18 +0100

    cars

--- cars/car.rb
+++ cars/car.rb
@@ -1,3 +1,11 @@
 class Car
-    def initialize()
-end
+    attr_accessor :paint
+  
+    def initialize(paint)
+         @paint = begin
+    end
+
+    def paint
+      puts "new color"
+    end
+  end
